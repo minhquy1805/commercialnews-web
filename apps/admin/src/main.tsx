ed2c3d3dd2511e454eba0f055a12fb55322a3bed
Minh Quy <minhquy073@gmail.com>
2026-05-05 20:56:00 +0700

    chore(admin): clean up Vite scaffold and configure app shell

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AppProviders } from "./app/providers/AppProviders";
+import App from "./App";
+import "antd/dist/reset.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </React.StrictMode>
+);
